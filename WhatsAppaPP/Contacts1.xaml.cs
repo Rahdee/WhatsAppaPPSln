359996596de0e33b8afd71c76548f9d630e75496
Rahdee <4283301@myuwc.ac.za>
2022-05-17 09:07:51 +0200

    added contact fontsize

--- WhatsAppaPP/Contacts1.xaml.cs
+++ WhatsAppaPP/Contacts1.xaml.cs
@@ -34,7 +34,7 @@ namespace WhatsAppaPP
         private void GenerateContracts()
         {
             var contact = new Contact();
-            contact.FullName = "Angel Adams";
+            contact.FullName = "Andre Adams";
             contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "074 463 1794";
 
@@ -42,98 +42,98 @@ namespace WhatsAppaPP
 
             contact = new Contact();
             contact.FullName = "Bonolo Mbekhi";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "063 467 1254";
 
             Contacts.Add(contact); 
 
             contact = new Contact();
             contact.FullName = "Britney Petersen";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "061 645 0901";
 
             Contacts.Add(contact); 
 
             contact = new Contact();
-            contact.FullName = "Chleo Adams";
-            contact.ProfileImageName = "stuff.png";
+            contact.FullName = "Chad Adams";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "064 024 5678";
 
             Contacts.Add(contact); 
 
             contact = new Contact();
             contact.FullName = "Cloe Petersen";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "075 897 1458";
 
             Contacts.Add(contact);
 
             contact = new Contact(); 
             contact.FullName = "Dominique Harris";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "065 890 1475";
 
             Contacts.Add(contact);
 
             contact = new Contact(); 
             contact.FullName = "Kayla Marcus";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "061 545 8702";
 
             Contacts.Add(contact);
                                                                 
             contact = new Contact(); 
             contact.FullName = "Lauren Peters";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "067 645 1951";
 
             Contacts.Add(contact);
 
             contact = new Contact();
             contact.FullName = "Mariam Johnson";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "061 445 5902";
 
             Contacts.Add(contact); 
 
             contact = new Contact();
-            contact.FullName = "Marilyn Paul";
-            contact.ProfileImageName = "stuff.png";
+            contact.FullName = "Marvin Paul";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "067 245 8901";
 
             Contacts.Add(contact);
 
             contact = new Contact(); 
             contact.FullName = "Meisha Peters";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "064 024 5678";
 
             Contacts.Add(contact);
 
             contact = new Contact();
-            contact.FullName = "Pan Smith";
-            contact.ProfileImageName = "stuff.png";
+            contact.FullName = "Peter Smith";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "071 097 2453";
 
             Contacts.Add(contact); 
 
             contact = new Contact();
             contact.FullName = "Robyn Davids";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "062 390 5475";
 
             Contacts.Add(contact);
 
             contact = new Contact();
             contact.FullName = "Sam Jameson";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "071 448 8712";
 
             Contacts.Add(contact);
 
             contact = new Contact();
             contact.FullName = "Tarryn Harris";
-            contact.ProfileImageName = "stuff.png";
+            contact.ProfileImageName = "Contacticon1.png";
             contact.PhoneNumber = "068 255 1151";
 
 
